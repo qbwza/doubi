55b5bc8c83f283b9d930ca48093b66913ec83573
qbwza <a397166878@gmail.com>
2019-03-06 23:31:58 +0800

    Update ssr.sh

--- ssr.sh
+++ ssr.sh
@@ -604,7 +604,7 @@ Debian_apt(){
 # 下载 ShadowsocksR
 Download_SSR(){
 	cd "/usr/local/"
-	wget -N --no-check-certificate "https://github.com/qbwza/qq/blob/master/manyuser.zip.zip"
+	wget -N --no-check-certificate "https://raw.githubusercontent.com/qbwza/qq/master/123/22"
 	#git config --global http.sslVerify false
 	#env GIT_SSL_NO_VERIFY=true git clone -b manyuser https://github.com/ToyoDAdoubi/shadowsocksr.git
 	#[[ ! -e ${ssr_folder} ]] && echo -e "${Error} ShadowsocksR服务端 下载失败 !" && exit 1
