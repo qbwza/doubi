abf0213bb50b5db7b59bd33ab0070dc0f9e9963d
qbwza <a397166878@gmail.com>
2019-03-07 01:23:56 +0800

    Update ssr.sh

--- ssr.sh
+++ ssr.sh
@@ -2,15 +2,10 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-#=================================================
-#	System Required: CentOS 6+/Debian 6+/Ubuntu 14.04+
-#	Description: Install the ShadowsocksR server
-#	Version: 2.0.37
-#	Author: Toyo
-#	Blog: https://doub.io/ss-jc42/
-#=================================================
+#================================================
+#================================================
 
-sh_ver="2.0.37"
+sh_ver="1.00"
 filepath=$(cd "$(dirname "$0")"; pwd)
 file=$(echo -e "${filepath}"|awk -F "$0" '{print $1}')
 ssr_folder="/usr/local/shadowsocksr"
